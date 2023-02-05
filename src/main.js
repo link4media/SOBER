180a570195f22551e0cbfac03e459a9a61c85960
Juan Carrera <link4media@gmail.com>
2023-02-05 15:59:46 -0500

    First script for scrolling header

--- src/main.js
+++ src/main.js
@@ -1,1 +1,33 @@
-const headerSwitch = document.querySelector('.header-1');
+// *** MOUSE OVER ***
+// document.querySelector('.header-change').addEventListener("mouseover", mouseOver);
+// document.querySelector('.header-change').addEventListener("mouseout", mouseOut);
+
+// function mouseOver() {
+//     document.querySelector('.header-1').style.display = "none";
+//     document.querySelector('.header-2').style.display = "block";
+// }
+// function mouseOut() {
+//     document.querySelector('.header-2').style.display = "none";
+//     document.querySelector('.header-1').style.display = "block";
+// }
+
+
+// document.querySelector('.header-1');
+// document.querySelector('.header-2');
+
+// onscroll = (event) => {
+//     document.querySelector('.header-1').style.display = "none";
+//     document.querySelector('.header-2').style.display = "block";
+// };
+
+var scrollTrigger = 50;
+
+window.onscroll = function() {
+  if (window.scrollY >= scrollTrigger || window.pageYOffset >= scrollTrigger) {
+    document.querySelector('.header-1').style.display = "none";
+    document.querySelector('.header-2').style.display = "block"
+  } else {
+    document.querySelector('.header-2').style.display = "none";
+    document.querySelector('.header-1').style.display = "block";
+  }
+};
